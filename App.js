cd6fa5997fb9b96235d5d4ed3b25b64baec3cad5
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-01-30 17:16:10 +0530

    Updated server temporarily

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const contactRoutes = require('./routes/Contact');
-const bodyParser = require('body-parser');
+const contactRoutes = require("./routes/Contact");
+const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use(contactRoutes);
 
-app.get('/', (req, res, next) => {
-    res.send('<h1>Welcome to my backend. Now git out!</h1>');
-})
-app.listen(process.env.PORT || 8080, console.log('Listening on port 8080'));
+app.get("/", (req, res, next) => {
+  res.send("<h1>Welcome to my backend. Now git out!</h1>");
+});
+
+mongoose
+  .connect(process.env.MONGODB_ID)
+  .then(() => console.log("Server started"))
+  .catch((err) => console.log("err is", err));
+app.listen(process.env.PORT || 8080, console.log("Listening on port 8080"));
--- controller/Contact.js
+++ controller/Contact.js
@@ -1,31 +1,16 @@
 require("dotenv").config();
-const nodemailer = require("nodemailer");
-const sendgridTransport = require("nodemailer-sendgrid-transport");
+const Message = require("../model/message");
 
 exports.postContact = async (req, res, next) => {
   const { firstName, lastName, email, message } = req.body;
 
-  const transporter = nodemailer.createTransport(
-    sendgridTransport({
-      auth: {
-        api_key: process.env.SENDGRID_KEY,
-      },
-    })
-  );
-
-  const info = await transporter.sendMail({
-    from: "aroravaibhav817@gmail.com",
-    to: email,
-    subject: "Thanks for reaching out",
-    html: `Hello ${firstName}, Thanks for reaching out. I have received your mail and will reach out to you as soon as I can 😀 <br /><br/>Thanks for having patience. <br/><br/>If your message is urgent don't hesitate to contact me on <a href = "https://twitter.com/Vaibhavtwts">Twitter</a> 🤝<br/><br/> Likewise you can also connect with me on <a href = "https://www.instagram.com/vaibhavarora.19/">Instagram</a> and we will have some chit-chat while eating noodles 🍜 with some nice tea ☕`,
+  const newMessage = new Message({
+    name: firstName + lastName,
+    email,
+    message,
   });
 
-  const details = await transporter.sendMail({
-    from: "aroravaibhav817@gmail.com",
-    to: "kingkratos767@gmail.com",
-    subject: "Vaibhav someone just contacted you!",
-    html: `Their name is ${firstName} ${lastName} \n Their email is ${email} \n Thier message is ${message}`,
-  });
+  await newMessage.save();
 
   res.send({ message: "Successfully sent" });
 };
